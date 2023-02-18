200b2d2a8ca148541a3f41363e6537091ab9e292
Johnny Mokolo <johnnyngotizo@hotmail.com>
2023-02-18 10:39:40 -0800

    2-assembler update

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
-*main entry
-*
-*return: always 0 (success)
-*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	return (0);
+    return (0);
 }
