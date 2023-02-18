54fff5ec43b88d631c9d54434bb2813c75105701
Johnny Mokolo <johnnyngotizo@hotmail.com>
2023-02-18 11:25:44 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print "Programming is like building a multilingual puzzle"
+ * main - print "Programming is like building a multilingual puzzle.
  *
- * Return 0.
+ * Return: Always 0.
  */
 
 int main(void)
