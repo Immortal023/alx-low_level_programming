0346486312f7f0caec1ea88d379671d7aafc1d6c
Johnny Mokolo <johnnyngotizo@hotmail.com>
2023-02-18 11:33:56 -0800

    update1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - print "Programming is like building a multilingual puzzle.
  *
@@ -11,5 +12,3 @@ int main(void)
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
-
-
