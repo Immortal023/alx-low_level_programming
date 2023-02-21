2788f9a32854a7bbc2d0e6392ce9796abcedf04c
Johnny Mokolo <johnnyngotizo@hotmail.com>
2023-02-21 00:07:29 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - Rand_Max / 2;
 if (n < 0)
 {
-printf("$d is positive\n", n);
+printf("n is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n", n);
+printf("n is zero\n", n);
 }
 else
 {
-printf("$d is negative\n", n);
+printf("n is negative\n", n);
 }
 return (0);
 }
