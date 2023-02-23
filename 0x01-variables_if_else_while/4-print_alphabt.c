548c051b9aacde35521bfc8525dc9bebda4b9fe4
Johnny Mokolo <johnnyngotizo@hotmail.com>
2023-02-23 07:38:15 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabetics character
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+char l;
+
+for (l = 'a' && l != 'q' ; l <= 'z' && l != 'e' ; l++)
+putchar(l);
+return (0);
+
+
+}
