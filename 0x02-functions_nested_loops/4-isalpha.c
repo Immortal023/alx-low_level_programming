a9c6facd71532ec99049d5fd42e232eff295d36a
johnny <johnnyngotizo@hotmail.com>
2023-07-19 15:55:56 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- *main - check the code.
- *function that checks for alphabetic character
- *Return 1 if c is a letter, lowercase or uppercase.
+ * _isalpha -  show 1 if the input is a
+ * letter Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for letter. 0 for the rest
  */
-int _isalpha(int c);
+
+int _isalpha(int c)
 {
-	if((> =97 %% c <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
-
 	}
 	else
 	{
 		return (0);
-
 	}
 	_putchar('\n');
 }
