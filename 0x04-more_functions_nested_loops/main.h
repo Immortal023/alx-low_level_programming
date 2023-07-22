4a44ea8dc0e467b8918f9c6bd6215e38387f720b
johnny <johnnyngotizo@hotmail.com>
2023-07-22 10:18:35 -0700

    issuper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,6 +9,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-
-#endif
