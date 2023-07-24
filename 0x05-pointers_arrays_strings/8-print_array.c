ad88621f772df9d7a206b59b036ad3762b8a46ed
johnny <johnnyngotizo@hotmail.com>
2023-07-24 13:14:48 -0700

    updated file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - a function that prints n elements of array
@@ -20,4 +21,3 @@ void print_array(int *a, int n)
 		}
 			printf("\n");
 }
-
